a40189768103fa0ebe7a08b4b514b5e9cc0f24a5
A-Daniel06 <danieldaniel6708@gmail.com>
2026-02-27 11:09:26 +0530

    Modify the connection

--- db.php
+++ db.php
@@ -5,7 +5,6 @@ $password = "5x2RiL2oe3";
 $database = "if0_41243771_sara_dj";
 
 $conn = mysqli_connect($host, $username, $password, $database);
-
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
